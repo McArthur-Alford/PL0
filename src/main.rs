901d09061a260ebaaeee854653258e952295b87f
McArthur-Alford <mcarthur.alford@bigpond.com>
2023-04-29 19:04:30 +1000

    things

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 mod parser;
 mod token;
 
@@ -9,11 +11,17 @@ enum OPCODE {
     POP,
     JMP,
     LDC(i32),
+    STF, // Store Frame
+    LDF, // Load Frame
+    BRT, // Branch True
+    BRF, // Branch False
+    WRT, // Write
     HALT
 }
 
 struct StackMachine {
     Stack: Vec<i32>,
+    Variables: HashMap<i32, i32>,
     Program: Vec<OPCODE>,
     PC: usize
 }
@@ -29,6 +37,7 @@ impl StackMachine {
     fn new() -> Self {
         Self {
             Stack: Vec::new(),
+            Variables: HashMap::new(),
             Program: Vec::new(),
             PC: 0
         }
@@ -81,6 +90,54 @@ impl StackMachine {
                 self.Stack.push(*x);
                 self.PC += 1;
                 Ok(None)
+            },
+            OPCODE::STF => {
+                let key = self.Stack.pop().ok_or("Stack Empty")?;
+                let value = self.Stack.pop().ok_or("Stack Empty")?;
+                self.Variables.insert(key, value);
+                self.PC += 1;
+                Ok(None)
+            },
+            OPCODE::LDF => {
+                let key = self.Stack.pop().ok_or("Stack Empty")?;
+                let value = self.Variables.get(&key).ok_or("Variable not found")?;
+                self.Stack.push(*value);
+                self.PC += 1;
+                Ok(None)
+            },
+            OPCODE::BRT => {
+                let offset = self.Stack.pop().ok_or("Stack Empty")?;
+                let condition = self.Stack.pop().ok_or("Stack Empty")?;
+                if condition != 0 {
+                    let newPC = (self.PC as i32 + offset);
+                    if newPC >= self.Program.len() as i32 || newPC < 0{
+                        return Err("Program Counter out of bounds".to_string());
+                    }
+                    self.PC = newPC as usize;
+                } else {
+                    self.PC += 1;
+                }
+                Ok(None)
+            },
+            OPCODE::BRF => {
+                let offset = self.Stack.pop().ok_or("Stack Empty")?;
+                let condition = self.Stack.pop().ok_or("Stack Empty")?;
+                if condition == 0 {
+                    let newPC = (self.PC as i32 + offset);
+                    if newPC >= self.Program.len() as i32 || newPC < 0{
+                        return Err("Program Counter out of bounds".to_string());
+                    }
+                    self.PC = newPC as usize;
+                } else {
+                    self.PC += 1;
+                }
+                Ok(None)
+            },
+            OPCODE::WRT => {
+                let value = self.Stack.pop().ok_or("Stack Empty")?;
+                print!("{}", value);
+                self.PC += 1;
+                Ok(Some(value))
             },
             OPCODE::HALT => {
                 Err("HALT".to_string())
@@ -167,5 +224,31 @@ mod tests {
         assert_eq!(results[1].Result, Ok(None));
         assert_eq!(results[2].Result, Ok(None));
         assert_eq!(results[3].Result, Ok(Some(2)));
+    }
+
+    #[test]
+    fn test_q2() {
+        /**
+         * var
+         *  x: int; i: int;
+         * begin
+         *  i := 0;
+         *  x := 0;
+         *  while i < 5 do
+         *   x := x + i;
+         *   i := i + 1
+         *  end;
+         * write x
+         * end
+         */
+        let mut machine = StackMachine::new();
+        machine.load(vec![
+            OPCODE::LDC(0), OPCODE::LDC(0), OPCODE::STF, // x := 0; at address 0
+            OPCODE::LDC(1), OPCODE::LDC(0), OPCODE::STF, // i := 0; at address 1
+
+        ]);
+        let results = machine.run();
+        print_results(results.clone());
+
     }
 }
